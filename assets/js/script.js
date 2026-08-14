676d4fb30fee952651fe1e8c50f06ce3bd5bb850
megha875 <meghaagarwal2436@gmail.com>
2026-08-14 15:46:13 +0530

    toogle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleSwitch = document.querySelector('#themeCheckbox');
+  const currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'light') {
+      toggleSwitch.checked = true;
+    }
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    }    
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
+});
+  
   //First Section
 
   (function () {
